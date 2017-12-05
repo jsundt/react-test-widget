730c13371dbdf5780a0ae0a29b40bee87b2a2168
Jørgen Sundt <jorgen@theeleven.co.uk>
2017-12-05 15:35:34 +0000

    Update description to include link to charliehr

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1>Welcome to the Test Widget: { this.props.title }</h1>
         </header>
         <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This test widget has some relevance to <a href="https://www.charliehr.com">CharlieHR</a>
         </p>
       </div>
     );
